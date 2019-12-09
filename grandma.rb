6315576633e3a5ea5d57c128676eb36efe47d83b
Robert M Ricci <rmricci121@gmail.com>
2019-12-09 17:31:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!".upcase
+  elsif phrase = "I LOVE YOU GRANDMA!".upcase
     return "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
